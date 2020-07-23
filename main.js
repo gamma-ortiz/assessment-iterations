71ca4836965e63e316bbddcace375c456ea1070c
gamma ortiz <gamma.ortiz@gmail.com>
2020-07-23 09:33:33 -0500

    finnito

---
+++ main.js
@@ -0,0 +1,9 @@
+// NOTE: A "users" array is already loaded.
+// See the "./data/users.js" script tag in index.html.
+
+// The following line is here just to show you that the
+// "users" array has already been loaded and is ready to go.
+console.log(users)
+
+// Append the katas to this element:
+const main = document.querySelector('main')
